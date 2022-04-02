cdb6226f3d42c39681c981d65ae2d18a2f56566d
Andre Hu <andrebhu@gmail.com>
2022-04-02 19:06:12 -0400

    Clears database per startup, cleanup

---
+++ flaskblog/create_db.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_login import LoginManager
+from flask import Flask
+
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+# Association Table for many-to-many relationship
+user_meeting = db.Table('user_meeting',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('meeting_id', db.Integer, db.ForeignKey('meetings.id'))
+)
+
+class Meeting(db.Model):
+    __tablename__ = 'meetings'
+    id = db.Column(db.Integer, primary_key=True)
+    start = db.Column(db.DateTime(), nullable=False)
+    end = db.Column(db.DateTime(), nullable=False)
+    # 'members' backref from User
+   
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(), nullable=False)
+    last_name = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    
+    # mentor, mentee, admin
+    account_type = db.Column(db.String(20), nullable=False)
+
+    interests = db.Column(db.String())
+    languages = db.Column(db.String())
+
+    # if User is a mentee, stores mentor
+    mentor_id = db.Column(db.Integer)
+
+    # storing mentees id in a space seperated string for now
+    # Ex:
+    # 1 4 20
+    mentees_id = db.Column(db.String())
+
+    # Connecting Users and Meetings with association table
+    meetings = db.relationship('Meeting', secondary=user_meeting, backref='members')
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+def init_db():
+    db.drop_all()
+    db.create_all()
+
+    admin = User()
+
+if __name__ == '__main__':
+    init_db()
+    print("Database created!")
--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 from flaskblog import app
+import subprocess
 
 if __name__ == '__main__':
+    subprocess.run(["./flaskblog/create_db.py"])
     app.run(debug=True)
