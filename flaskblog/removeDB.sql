e1eed37f2970e4abf8ff0a5cab2c7d75d0bfba6c
Shikai Wang <seuwsk@gmail.com>
2022-04-02 13:05:10 -0400

    build up tables

---
+++ flaskblog/removeDB.sql
@@ -0,0 +1,6 @@
+drop table if exists mentee;
+drop table if exists mentor;
+drop table if exists mentee_group;
+drop table if exists meeting;
+drop table if exists language;
+drop table if exists interest;
