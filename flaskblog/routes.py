ba26d60ad56e4aa2ed60137b17cf28ccc17a9c6c
Andre Hu <andrebhu@gmail.com>
2022-04-03 19:14:19 -0400

    Remove about page

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -15,12 +15,7 @@ from loguru import logger
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('index.html')
-
-
-@app.route("/about")
-def about():
-    return render_template('about.html', title='About')
+    return render_template('index.html', title="Tech Alpharetta")
 
 
 # Registration for mentees
