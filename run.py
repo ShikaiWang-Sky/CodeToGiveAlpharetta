90d9896d7aadb9a48c341e3dcb12cc9d239236a2
Andre Hu <andrebhu@gmail.com>
2022-04-03 16:15:12 -0400

    Make run.py compatible with Windows

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from flaskblog import app
 import subprocess
 
 if __name__ == '__main__':
-    subprocess.run(["./flaskblog/create_db.py"])
+    subprocess.run(["python3 flaskblog/create_db.py"], shell=True)
     app.run(debug=True)
